01c96164a3e4623a70e967d9eeeda4799ab24114
bhanu4snowflake <143876121+bhanu4snowflake@users.noreply.github.com>
2023-09-02 16:36:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 
 import streamlit
-streamlit.header('🥣 Breakfast Menu')
+streamlit.header('🥣 Breakfast favourites')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑Hard-Boiled Free-Range Egg')
 
-🥣 🥗 🐔 🥑🍞
+
