c305738124ecf4246305249ef64f368287289742
bhanu4snowflake <143876121+bhanu4snowflake@users.noreply.github.com>
2023-09-08 17:24:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import snowflake.connector
 
 streamlit.header("Fruityvice Fruit Advice!")
 #try:  
-     fruit_choice = streamlit.text_input('What fruit would you like information about?')
+      streamlit.text_input('What fruit would you like information about?')
 #if fruit_choice:
     #   streamlit.error("Please select a fruit to get information.")
 #else:
